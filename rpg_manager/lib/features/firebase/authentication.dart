95f6fbf1e9d6a610c61e10ef6d0b57b6b9d3a4e3
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-09-29 00:26:49 +0200

    Create authentication.dart

---
+++ rpg_manager/lib/features/firebase/authentication.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthentication {
+  FirebaseAuthentication(this._auth);
+
+  final FirebaseAuth _auth;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  void authenticationChangesListener() {
+    _auth.authStateChanges().listen((user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  Future<String?> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'Successfully Logged In :)';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return e.message;
+    }
+  }
+
+  Future<String?> register(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'Successfully registration :)';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      return e.message;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}

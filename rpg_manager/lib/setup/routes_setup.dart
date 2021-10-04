796d0dc190426ca948e0f704c477ffa4ab97d300
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-10-04 22:13:17 +0200

    Update routes_setup.dart

--- rpg_manager/lib/setup/routes_setup.dart
+++ rpg_manager/lib/setup/routes_setup.dart
@@ -3,6 +3,11 @@ import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rpg_manager/app.dart';
 import 'package:rpg_manager/features/authorization/register/register.dart';
+import 'package:rpg_manager/features/calendar/calendar.dart';
+import 'package:rpg_manager/features/campaigns/campaigns.dart';
+import 'package:rpg_manager/features/home/home.dart';
+import 'package:rpg_manager/features/main_menu/main_menu.dart';
+import 'package:rpg_manager/features/players_list/players_list.dart';
 import 'package:rpg_manager/widgets/app_background.dart';
 import 'package:rpg_manager/widgets/app_nav_bar.dart';
 
@@ -15,6 +20,16 @@ class RoutesSetup {
         return MaterialPageRoute(builder: (_) => StartPage());
       case RoutePageName.register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case RoutePageName.home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+      case RoutePageName.menu:
+        return MaterialPageRoute(builder: (_) => MainMenu());
+      case RoutePageName.calendarPage:
+        return MaterialPageRoute(builder: (_) => CalendarScreen());
+      case RoutePageName.campaignsPage:
+        return MaterialPageRoute(builder: (_) => CampaignsScreen());
+      case RoutePageName.playersListPage:
+        return MaterialPageRoute(builder: (_) => PlayersListScreen());
       default:
         return _errorRoute();
     }
@@ -48,4 +63,8 @@ class RoutePageName {
   static const String register = '/register';
   static const String startPage = '/';
   static const String home = '/home';
+  static const String menu = '/menu';
+  static const String calendarPage = '/calendar';
+  static const String campaignsPage = '/campaigns';
+  static const String playersListPage = '/players';
 }

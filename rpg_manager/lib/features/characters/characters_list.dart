266721868119127bc704552cf2cc95dbae9db27d
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-10-04 23:48:06 +0200

    Create characters_list.dart

---
+++ rpg_manager/lib/features/characters/characters_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CharactersListScreen extends StatefulWidget {
+  @override
+  _CharactersListScreenState createState() => _CharactersListScreenState();
+}
+
+class _CharactersListScreenState extends State<CharactersListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Lista kart postaci'),
+    );
+  }
+}

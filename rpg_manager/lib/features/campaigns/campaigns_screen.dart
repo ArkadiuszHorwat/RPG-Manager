efa6afcfecb8c8b9b9d09db05ee6e06efa3719e0
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-10-15 21:51:57 +0200

    Update campaigns_screen.dart

--- rpg_manager/lib/features/campaigns/campaigns_screen.dart
+++ rpg_manager/lib/features/campaigns/campaigns_screen.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rpg_manager/features/campaigns/widgets/campaigns_list.dart';
+
+final testCampaignsList = [
+  'Zapomniana kopalnia Phandelvera',
+  'Język smoka',
+  'Wrota Baldura',
+  'Wrota Baldura',
+  'Wrota Baldura',
+  'Wrota Baldura',
+  'Wrota Baldura',
+];
 
 class CampaignsScreen extends StatefulWidget {
   @override
@@ -15,12 +26,7 @@ class _CampaignsScreenState extends State<CampaignsScreen> {
         _addCampaign(),
         Expanded(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                _listItem('asd'),
-                _listItem('asd'),
-              ],
-            ),
+            child: CampaignsList(data: testCampaignsList),
           ),
         ),
       ],
@@ -29,12 +35,11 @@ class _CampaignsScreenState extends State<CampaignsScreen> {
 
   Widget _addCampaign() {
     return Container(
-      color: Color.fromARGB(255, 247, 241, 227),
-      margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+      alignment: AlignmentDirectional.centerEnd,
       child: TextButton(
         onPressed: () {},
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Padding(
               padding: const EdgeInsets.all(5.0),
@@ -44,47 +49,18 @@ class _CampaignsScreenState extends State<CampaignsScreen> {
                   textStyle: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
-                    color: Color.fromARGB(255, 168, 128, 92),
+                    color: Color.fromARGB(255, 247, 241, 227),
                   ),
                 ),
               ),
             ),
             Icon(
               CupertinoIcons.plus_app_fill,
-              color: Color.fromARGB(255, 168, 128, 92),
+              color: Color.fromARGB(255, 247, 241, 227),
             ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _listItem(String text) {
-    return Column(
-      children: [
-        ListTile(
-          tileColor: Color.fromARGB(150, 0, 0, 0),
-          onTap: () {},
-          title: Text(
-            text,
-            style: GoogleFonts.rubik(
-              textStyle: TextStyle(
-                fontSize: 16,
-                color: Color.fromARGB(255, 247, 241, 227),
-              ),
-            ),
-          ),
-          trailing: Icon(
-            Icons.info_outline,
-            color: Color.fromARGB(255, 247, 241, 227),
-          ),
-        ),
-        Divider(
-          color: Color.fromARGB(255, 247, 241, 227),
-          thickness: 1,
-          height: 0,
-        ),
-      ],
     );
   }
 }

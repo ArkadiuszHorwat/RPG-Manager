d85080866ba1bd35562cf35d999982ad28e03714
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-10-15 21:52:10 +0200

    Create camaigns_card_item.dart

---
+++ rpg_manager/lib/features/campaigns/widgets/camaigns_card_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CampaignsCardItem extends StatelessWidget {
+  const CampaignsCardItem({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {},
+      child: Container(
+        alignment: AlignmentDirectional.center,
+        margin: EdgeInsets.all(10),
+        height: 150,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 3,
+              spreadRadius: 1,
+            ),
+          ],
+          borderRadius: BorderRadiusDirectional.all(Radius.circular(30)),
+          image: new DecorationImage(
+            fit: BoxFit.cover,
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.3), BlendMode.dstATop),
+            image: new AssetImage(
+              'lib/app_assets/images/gracz-img.jpg',
+            ),
+          ),
+        ),
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.rubik(
+            textStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 247, 241, 227),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

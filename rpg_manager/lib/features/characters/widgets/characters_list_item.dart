d12eef5f1654c12c38d3003bcd0e59c777701d32
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-10-16 15:52:51 +0200

    Create characters_list_item.dart

---
+++ rpg_manager/lib/features/characters/widgets/characters_list_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CharacterListItem extends StatelessWidget {
+  CharacterListItem({
+    required this.characterName,
+  });
+
+  final String characterName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.fromLTRB(50, 0, 0, 0),
+          alignment: AlignmentDirectional.centerStart,
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 3,
+                spreadRadius: 1,
+              ),
+            ],
+            borderRadius: BorderRadiusDirectional.only(
+              topStart: Radius.circular(10),
+              bottomStart: Radius.circular(10),
+            ),
+            image: new DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: new ColorFilter.mode(
+                  Colors.black.withOpacity(0.3), BlendMode.dstATop),
+              image: new AssetImage(
+                'lib/app_assets/images/gracz-img.jpg',
+              ),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 10, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  characterName,
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 247, 241, 227),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  color: Color.fromARGB(255, 247, 241, 227),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

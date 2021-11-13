3adb984c345581b83a00cf2c8487b57515172b4f
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-11-13 21:55:06 +0100

    add edit bio

--- rpg_manager/lib/features/characters/character_details/widgets/characterBio.dart
+++ rpg_manager/lib/features/characters/character_details/widgets/characterBio.dart
@@ -1,9 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rpg_manager/app_assets/colors/colors.dart';
 
 class CharacterBio extends StatelessWidget {
+  CharacterBio({
+    required this.characterId,
+    required this.characterBio,
+  });
+
+  final String characterId;
+  final String characterBio;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,63 +20,161 @@ class CharacterBio extends StatelessWidget {
         SizedBox(
           height: 10,
         ),
-        Container(
-          height: 200,
-          decoration: BoxDecoration(
-            color: AppColors.appLight,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black,
-                blurRadius: 1,
-                spreadRadius: 1,
-              ),
-            ],
-            borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Biografia',
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        color: AppColors.appDark,
-                        fontSize: 14.0,
+        GestureDetector(
+          onTap: () {
+            _bioEditHandle(context);
+          },
+          child: Container(
+            height: 200,
+            decoration: BoxDecoration(
+              color: AppColors.appLight,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 1,
+                  spreadRadius: 1,
+                ),
+              ],
+              borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Biografia',
+                      style: GoogleFonts.rubik(
+                        textStyle: TextStyle(
+                          color: AppColors.appDark,
+                          fontSize: 14.0,
+                        ),
                       ),
                     ),
-                  ),
-                  Divider(
-                    color: AppColors.appDark,
-                  ),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit erat vel pretium mollis.'
-                    'Donec nec neque non quam ultrices imperdiet ac vel arcu. Aenean magna justo, efficitur porta volutpat at,'
-                    'consequat eu lorem. Vivamus mattis elementum urna, in ullamcorper urna eleifend eget. Praesent sagittis sagittis neque,'
-                    'a egestas enim finibus non. Phasellus nisi leo, ullamcorper ut tempor vitae, mattis venenatis arcu. Nam eget justo sodales,'
-                    'sollicitudin metus eu, suscipit odio. Morbi tempus faucibus augue, eget tristique leo porttitor vel. Vestibulum condimentum'
-                    'velit vitae ipsum laoreet lobortis. Sed tempor felis quis interdum aliquam.'
-                    'Integer metus felis, vestibulum ac cursus a, rutrum quis nisl. Nullam congue, massa a ultricies aliquet,'
-                    'metus nunc tincidunt mauris, vel convallis sapien lacus quis diam.',
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        color: AppColors.appDark,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+                    Divider(
+                      color: AppColors.appDark,
+                    ),
+                    Text(
+                      characterBio,
+                      style: GoogleFonts.rubik(
+                        textStyle: TextStyle(
+                          color: AppColors.appDark,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Divider(
-                    color: AppColors.appDark,
-                  ),
-                ],
+                    Divider(
+                      color: AppColors.appDark,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ],
     );
+  }
+
+  void _bioEditHandle(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final formKey = GlobalKey<FormState>();
+          final textController = TextEditingController();
+          textController.text = characterBio;
+
+          return AlertDialog(
+            backgroundColor: AppColors.appLight,
+            title: Column(
+              children: [
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Text(
+                  'Edytuj biografie',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Container(
+                  height: 250,
+                  child: Form(
+                    key: formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: TextFormField(
+                      maxLines: 15,
+                      controller: textController,
+                      keyboardType: TextInputType.multiline,
+                      autofocus: false,
+                      validator: (text) {
+                        if (text != null) {
+                          print('fajen');
+                        } else {
+                          print('niefajen');
+                          return '';
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  try {
+                    CollectionReference characters =
+                        FirebaseFirestore.instance.collection('characters');
+
+                    characters
+                        .doc('$characterId')
+                        .update({'bio': textController.text})
+                        .then((value) => print('bio was updated'))
+                        .catchError((error) => print('Failed to update bio'));
+                  } on Exception catch (e) {
+                    print('Coś poszło nie tak: $e');
+                  }
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Zapisz',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Wróć',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }

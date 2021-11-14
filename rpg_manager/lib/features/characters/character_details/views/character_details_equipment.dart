dac9e245b14bc713a7a730dcce573a4aba643639
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-11-14 21:15:42 +0100

    Update character_details_equipment.dart

--- rpg_manager/lib/features/characters/character_details/views/character_details_equipment.dart
+++ rpg_manager/lib/features/characters/character_details/views/character_details_equipment.dart
@@ -1,20 +1,422 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rpg_manager/app_assets/colors/colors.dart';
 
 class CharacterDetailsEquipment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Ekwipunek',
-        style: GoogleFonts.rubik(
-          textStyle: TextStyle(
-            color: AppColors.appLight,
-            fontSize: 28.0,
-          ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            _wyposazenie(context),
+            _ekwipunek(context),
+          ],
         ),
       ),
     );
+  }
+
+  Widget _ekwipunek(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 350,
+          decoration: BoxDecoration(
+            color: AppColors.appLight,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 1,
+                spreadRadius: 1,
+              ),
+            ],
+            borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ekwipunek',
+                          style: GoogleFonts.rubik(
+                            textStyle: TextStyle(
+                              color: AppColors.appDark,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          color: AppColors.appDark,
+                        ),
+                        Text(
+                          'asdasda',
+                          style: GoogleFonts.rubik(
+                            textStyle: TextStyle(
+                              color: AppColors.appDark,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          color: AppColors.appDark,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _money(),
+                      _money(),
+                      _money(),
+                      _money(),
+                      _money(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _money() {
+    return Container(
+      alignment: AlignmentDirectional.center,
+      height: 50,
+      width: 80,
+      decoration: BoxDecoration(
+        color: AppColors.appDark,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 3,
+            spreadRadius: 0.5,
+          ),
+        ],
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'SS',
+              style: GoogleFonts.rubik(
+                textStyle: TextStyle(
+                  color: AppColors.appLight,
+                  fontSize: 11.0,
+                ),
+              ),
+            ),
+            Text(
+              '50',
+              style: GoogleFonts.rubik(
+                textStyle: TextStyle(
+                  color: AppColors.appLight,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _wyposazenie(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 400,
+          decoration: BoxDecoration(
+            color: AppColors.appLight,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 1,
+                spreadRadius: 1,
+              ),
+            ],
+            borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Wyposażenie',
+                    style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                        color: AppColors.appDark,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: AppColors.appDark,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          'nazwa',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.rubik(
+                            textStyle: TextStyle(
+                              color: AppColors.appDark,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          'premia do ataku',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.rubik(
+                            textStyle: TextStyle(
+                              color: AppColors.appDark,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          'obrażenia/typ',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.rubik(
+                            textStyle: TextStyle(
+                              color: AppColors.appDark,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  _equipElement(),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  _equipElement(),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  _equipElement(),
+                  Divider(
+                    color: AppColors.appDark,
+                  ),
+                  Text(
+                    'asd',
+                    style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                        color: AppColors.appDark,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: AppColors.appDark,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _equipElement() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.appDark,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 1,
+                  spreadRadius: 0.5,
+                ),
+              ],
+            ),
+            height: 40,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.appDark,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 1,
+                  spreadRadius: 0.5,
+                ),
+              ],
+            ),
+            height: 40,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColors.appDark,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 1,
+                  spreadRadius: 0.5,
+                ),
+              ],
+            ),
+            height: 40,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _bioEditHandle(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final formKey = GlobalKey<FormState>();
+          final textController = TextEditingController();
+          textController.text = '';
+
+          return AlertDialog(
+            backgroundColor: AppColors.appLight,
+            title: Column(
+              children: [
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Text(
+                  'Edytuj wyposażenie',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Container(
+                  height: 250,
+                  child: Form(
+                    key: formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: TextFormField(
+                      maxLines: 15,
+                      controller: textController,
+                      keyboardType: TextInputType.multiline,
+                      autofocus: false,
+                      validator: (text) {
+                        if (text != null) {
+                          print('fajen');
+                        } else {
+                          print('niefajen');
+                          return '';
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  try {
+                    print('asd');
+                  } on Exception catch (e) {
+                    print('Coś poszło nie tak: $e');
+                  }
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Zapisz',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Wróć',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }

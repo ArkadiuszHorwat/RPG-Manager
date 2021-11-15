d8413e22ed6a09b57ec9793b52c4b798affe4eb4
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-11-15 14:33:40 +0100

    Update character_details_skills.dart

--- rpg_manager/lib/features/characters/character_details/views/character_details_skills.dart
+++ rpg_manager/lib/features/characters/character_details/views/character_details_skills.dart
@@ -3,49 +3,187 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rpg_manager/app_assets/colors/colors.dart';
 
+final skills = [
+  'Akrobatyka',
+  'Atletyka',
+  'Historia',
+  'Intuicja',
+  'Medycyna',
+  'Opieka nad zwierzętami',
+  'Oszustwo',
+  'Percepcja',
+  'Perswazja',
+  'Przyroda',
+  'Religia',
+  'Skradanie się',
+  'Sztuka przetrwania',
+  'Śledztwo',
+  'Wiedza tajemna',
+  'Występy',
+  'Zastraszenie',
+  'Zwinne dłonie',
+];
+
+final savingThrows = [
+  'Siła',
+  'Zręczność',
+  'Kondycja',
+  'Inteligencja',
+  'Mądrość',
+  'Charyzma',
+];
+
+final atributes = [
+  'Zrc',
+  'Sil',
+  'Int',
+  'Mdr',
+  'Cha',
+];
+
 class CharacterDetailsSkills extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _characterInfoCell('Inspiracja', '1'),
+                  _characterInfoCell('Szybkość', '9'),
+                  _characterInfoCell('Inicjatywa', '+3'),
+                  _characterInfoCell('KP', '16'),
+                ],
+              ),
+            ),
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _characterInfoCell('PW (max)', '27'),
+                  _characterInfoCell('PW (temp)', '11'),
+                  _characterInfoCell('Inicjatywa', '+3'),
+                  _characterInfoCell('KW', 'k10'),
+                ],
+              ),
+            ),
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _characterInfoCell('pr. z bieg.', '+2'),
+                  _characterInfoCell('percepcja', '14'),
+                  _characterInfoCell('Inicjatywa', '+3'),
+                  _characterInfoCell('KW', 'k10'),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: _infoContainer('Rzuty obronne', savingThrows),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: _infoContainer('Umiejętności', skills),
+                ),
+              ],
+            ),
+            _infoContainer('Biegłości i języki', ['asda', 'asda', 'asd']),
+            _infoContainer('Zdolności', ['asda', 'asda', 'asd']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _infoContainer(String title, List<String> items) {
+    final listItem = <Widget>[];
+    items.forEach((item) => listItem.add(_listElement(item)));
     return Column(
       children: [
-        Text(
-          'Umiejętności',
-          style: GoogleFonts.rubik(
-            textStyle: TextStyle(
-              color: AppColors.appLight,
-              fontSize: 28.0,
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 350,
+          decoration: BoxDecoration(
+            color: AppColors.appLight,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 1,
+                spreadRadius: 1,
+              ),
+            ],
+            borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                        color: AppColors.appDark,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: listItem,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        Container(
-          alignment: AlignmentDirectional.centerEnd,
-          height: 150,
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _characterInfoCell('Inspiracja', '1'),
-              _characterInfoCell('Szybkość', '9'),
-              _characterInfoCell('Inicjatywa', '+3'),
-              _characterInfoCell('KP', '16'),
-            ],
+      ],
+    );
+  }
+
+  Widget _listElement(String name) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          name,
+          style: GoogleFonts.rubik(
+            textStyle: TextStyle(
+              color: AppColors.appDark,
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        Container(
-          alignment: AlignmentDirectional.centerEnd,
-          height: 150,
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _characterInfoCell('PW (max)', '27'),
-              _characterInfoCell('PW (temp)', '11'),
-              _characterInfoCell('Inicjatywa', '+3'),
-              _characterInfoCell('KW', 'k10'),
-            ],
-          ),
+        Divider(
+          color: AppColors.appDark,
         ),
       ],
     );
@@ -72,8 +210,8 @@ class CharacterDetailsSkills extends StatelessWidget {
             height: 10,
           ),
           Container(
-            clipBehavior: Clip.none,
-            height: 70,
+            alignment: AlignmentDirectional.center,
+            height: 60,
             width: 60,
             decoration: BoxDecoration(
               color: AppColors.appLight,
@@ -86,8 +224,7 @@ class CharacterDetailsSkills extends StatelessWidget {
               ],
               borderRadius: BorderRadiusDirectional.all(Radius.circular(20)),
             ),
-            child: Center(
-                child: Text(
+            child: Text(
               count,
               style: GoogleFonts.rubik(
                 textStyle: TextStyle(
@@ -95,7 +232,7 @@ class CharacterDetailsSkills extends StatelessWidget {
                   fontSize: 24.0,
                 ),
               ),
-            )),
+            ),
           ),
         ],
       ),

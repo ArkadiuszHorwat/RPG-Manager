de9f35d108757f1736fc68c38fd188f36eaab712
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-12-05 17:29:30 +0100

    Create skills_model.dart

---
+++ rpg_manager/lib/features/characters/models/skills_model.dart
@@ -0,0 +1,62 @@
+class SkillsModel {
+  SkillsModel({
+    this.acrobatics,
+    this.animalHandling,
+    this.arcana,
+    this.athletics,
+    this.deception,
+    this.history,
+    this.insight,
+    this.intimidation,
+    this.investigation,
+    this.medicine,
+    this.nature,
+    this.perception,
+    this.performance,
+    this.persuasion,
+    this.religion,
+    this.sleightOfHand,
+    this.stealth,
+    this.survival,
+  });
+
+  final String? acrobatics;
+  final String? animalHandling;
+  final String? arcana;
+  final String? athletics;
+  final String? deception;
+  final String? history;
+  final String? insight;
+  final String? intimidation;
+  final String? investigation;
+  final String? medicine;
+  final String? nature;
+  final String? performance;
+  final String? persuasion;
+  final String? religion;
+  final String? sleightOfHand;
+  final String? stealth;
+  final String? survival;
+  final String? perception;
+
+  final skills = [
+    'Akrobatyka (Zrc)',
+    'Atletyka (Sil)',
+    'Historia (Int)',
+    'Intuicja (Mdr)',
+    'Medycyna (Mdr)',
+    'Opieka nad zwierzętami (Mdr)',
+    'Oszustwo (Cha)',
+    'Percepcja (Mdr)',
+    'Perswazja (Cha)',
+    'Przyroda (Int)',
+    'Religia (Int)',
+    'Skradanie się (Zrc)',
+    'Sztuka przetrwania (Mdr)',
+    'Śledztwo (Int)',
+    'Wiedza tajemna (Int)',
+    'Występy (Cha)',
+    'Zastraszenie (Cha)',
+    'Zwinne dłonie (Zrc)',
+  ];
+}

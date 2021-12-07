08befcb8e535d491826136d7cdb53e4f9ea71614
ArkadiuszHorwat <62152378+ArkadiuszHorwat@users.noreply.github.com>
2021-12-08 00:18:21 +0100

    add description edit method

--- rpg_manager/lib/features/campaigns/campaign_details/campaign_details_screen_controller.dart
+++ rpg_manager/lib/features/campaigns/campaign_details/campaign_details_screen_controller.dart
@@ -35,21 +35,135 @@ class CampaignDetailsScreenController {
     }).catchError((_) => errorDialog(context));
   }
 
-  void updateSessionStatus({
+  void updateCampaign({
     required String campaignId,
-    required bool sessionStatus,
+    required dynamic newValue,
+    required String updateTargetName,
   }) {
     try {
       campaigns
           .doc(campaignId)
           .update({
-            'sessionStatus': sessionStatus,
+            updateTargetName: newValue,
           })
-          .then((value) => print('session status was update'))
-          .catchError((error) => print('Failed to update session status'));
+          .then((value) => print('$updateTargetName was update'))
+          .catchError((error) => print('Failed to update $updateTargetName'));
     } on Exception catch (e) {
       print('Coś poszło nie tak: $e');
     }
+  }
+
+  void descriptionEditHandle(
+    BuildContext context, {
+    required String title,
+    String? hintText,
+    required String updateTargetName,
+    String? campaignId,
+    String? campaignMultiText,
+  }) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final textController = TextEditingController();
+          final formKey = GlobalKey<FormState>();
+
+          if (campaignMultiText != null) {
+            textController.text = campaignMultiText;
+          }
+
+          return AlertDialog(
+            backgroundColor: AppColors.appLight,
+            title: Column(
+              children: [
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Text(
+                  title,
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+                Container(
+                  height: campaignMultiText != null ? 250 : 50,
+                  child: Form(
+                    key: formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: TextFormField(
+                      controller: textController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 15,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: hintText,
+                        hintStyle: GoogleFonts.rubik(
+                          textStyle: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      autofocus: false,
+                      validator: (text) {
+                        if (text != null) {
+                          print('fajen');
+                        } else {
+                          print('niefajen');
+                          return '';
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.appDark,
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  updateCampaign(
+                    campaignId: campaignId ?? '',
+                    updateTargetName: updateTargetName,
+                    newValue: textController.text,
+                  );
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Zapisz',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Wróć',
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      color: AppColors.appDark,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 
   void _deleteCampaign({required String campaignId}) {
